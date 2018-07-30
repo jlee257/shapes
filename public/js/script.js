02e820a44ba429dc5f49ba312a2473364e00c2e2
Sanlok Lee <jlee257@berkeley.edu>
2018-07-30 00:10:50 -0700

    curve estimation logic update

--- public/js/script.js
+++ public/js/script.js
@@ -1311,15 +1311,35 @@ $(document).ready(function () {
     var errz = 0; // error from curve to the closest point
     for (var i = 0, ilen = points.length; i < ilen; i++) {
       var e = 3000*3000;
+      var closest_j = -1;
       for (var j = 0, jlen = curve_points.length; j < jlen; j++) {
-
         var dist = distanceBetweenSquared(points[i].x, points[i].y, curve_points[j].x, curve_points[j].y);
-
         curve_points[j].z = Math.min(dist, curve_points[j].z);
-        e = Math.min(e, dist);
+        if (dist < e) {
+          e = dist;
+          closest_j = j;
+        }
       }
 
-      err += e;
+      if (closest_j == 0 || closest_j == curve_points.length - 1) {
+        e = distanceBetween(curve_points[closest_j].x, curve_points[closest_j].y, points[i].x, points[i].y);
+        console.log("Curve: point=" + i + " curve=" + closest_j + " err=" + e);
+      } else if (0 < closest_j && closest_j < curve_points.length - 1) {
+        var dis_a = distanceFromLine(curve_points[closest_j-1].x, curve_points[closest_j-1].y, curve_points[closest_j].x, curve_points[closest_j].y, points[i].x, points[i].y);
+        var dis_b = distanceFromLine(curve_points[closest_j].x, curve_points[closest_j].y, curve_points[closest_j+1].x, curve_points[closest_j+1].y, points[i].x, points[i].y);
+        var dis_c = distanceBetween(curve_points[closest_j].x, curve_points[closest_j].y, points[i].x, points[i].y);
+        e = Math.min(dis_a, dis_b, dis_c);
+        console.log("Curve: point=" + i + " curve=" + closest_j + " dis_a=" + dis_a + " dis_b=" + dis_b + " dis_c=" + dis_c + " err=" + e);
+      // } else if (closest_j == curve_points.length - 1){
+      //   var dis_a = distanceFromLine(curve_points[closest_j-1].x, curve_points[closest_j-1].y, curve_points[closest_j].x, curve_points[closest_j].y, points[i].x, points[i].y);
+      //   var dis_c = distanceBetween(curve_points[closest_j].x, curve_points[closest_j].y, points[i].x, points[i].y);
+      //   e = Math.min(dis_a, dis_c);
+      //   console.log("Curve: point=" + i + " curve=" + closest_j + " dis_a=" + dis_a + " dis_c=" + dis_c + " err=" + e);
+      } else {
+        e = 3000;
+        console.log("Curve: point=" + i + " curve=" + closest_j + " err=" + e);
+      }
+      err += e**2;
     }
 
     for (var j = 0, jlen = curve_points.length; j < jlen; j++) {
@@ -1351,60 +1371,6 @@ $(document).ready(function () {
     $("#canvas-score-text").css("color", color);
     $("#canvas-score").css("color", color);
     $("#canvas-score").html("Score: " + score.toFixed(1));
-  }
-
-  function cuberoot(x) {
-    var y = Math.pow(Math.abs(x), 1/3);
-    return x < 0 ? -y : y;
-  }
-
-  function solveCubic(a, b, c, d) {
-    if (Math.abs(a) < 1e-8) { // Quadratic case, ax^2+bx+c=0
-        a = b; b = c; c = d;
-        if (Math.abs(a) < 1e-8) { // Linear case, ax+b=0
-            a = b; b = c;
-            if (Math.abs(a) < 1e-8) // Degenerate case
-                return [];
-            return [-b/a];
-        }
-
-        var D = b*b - 4*a*c;
-        if (Math.abs(D) < 1e-8)
-            return [-b/(2*a)];
-        else if (D > 0)
-            return [(-b+Math.sqrt(D))/(2*a), (-b-Math.sqrt(D))/(2*a)];
-        return [];
-    }
-
-    // Convert to depressed cubic t^3+pt+q = 0 (subst x = t - b/3a)
-    var p = (3*a*c - b*b)/(3*a*a);
-    var q = (2*b*b*b - 9*a*b*c + 27*a*a*d)/(27*a*a*a);
-    var roots;
-
-    if (Math.abs(p) < 1e-8) { // p = 0 -> t^3 = -q -> t = -q^1/3
-        roots = [cuberoot(-q)];
-    } else if (Math.abs(q) < 1e-8) { // q = 0 -> t^3 + pt = 0 -> t(t^2+p)=0
-        roots = [0].concat(p < 0 ? [Math.sqrt(-p), -Math.sqrt(-p)] : []);
-    } else {
-        var D = q*q/4 + p*p*p/27;
-        if (Math.abs(D) < 1e-8) {       // D = 0 -> two roots
-            roots = [-1.5*q/p, 3*q/p];
-        } else if (D > 0) {             // Only one real root
-            var u = cuberoot(-q/2 - Math.sqrt(D));
-            roots = [u - p/(3*u)];
-        } else {                        // D < 0, three roots, but needs to use complex numbers/trigonometric solution
-            var u = 2*Math.sqrt(-p/3);
-            var t = Math.acos(3*q/p/u)/3;  // D < 0 implies p < 0 and acos argument in [-1..1]
-            var k = 2*Math.PI/3;
-            roots = [u*Math.cos(t), u*Math.cos(t-k), u*Math.cos(t-2*k)];
-        }
-    }
-
-    // Convert back from depressed cubic
-    for (var i = 0; i < roots.length; i++)
-        roots[i] -= b/(3*a);
-
-    return roots;
   }
 
   function drawRandomEllipse() {
